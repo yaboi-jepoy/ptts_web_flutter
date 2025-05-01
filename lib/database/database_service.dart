6a8002534a61a35140e36afbc6c2b064503527f0
tnznt <qjpapenas@tip.edu.ph>
2025-05-01 15:45:29 +0800

    admin page now working as intendec
    YIPEEEE!!!

--- lib/database/database_service.dart
+++ lib/database/database_service.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
@@ -29,41 +31,122 @@ class DatabaseService {
     final database = await openDatabase(
       databasePath,
       version: 1,
+      onConfigure: (db) async {
+        await db.execute(
+          'PRAGMA foreign_keys = ON',
+        ); // turn on your foreign keys for god's sake
+      },
       onCreate: (db, version) {
         // create table like SQL query
-        // vehicles
-        db.execute(
-          'CREATE TABLE $_vehiclesTableName(vehicle_no INTEGER PRIMARY KEY, plate_no TEXT NOT NULL, capacity INT, vehicle_type TEXT)',
-        );
-        // drivers
-        db.execute(
-          'CREATE TABLE $_driversTableName(driver_no INTEGER PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, age TEXT)',
-        );
-        // conductors
-        db.execute(
-          'CREATE TABLE $_conductorsTableName(conductor_no INTEGER PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL, age TEXT)',
-        );
-
-        // TODO - these two tables will be updated later
         // terminal
         db.execute(
-          'CREATE TABLE $_terminalsTableName(terminal_id INT PRIMARY KEY, status TEXT NOT NULL, vehicle_no INTEGER, available INTEGER)',
+          'CREATE TABLE $_terminalsTableName(terminal_id INTEGER PRIMARY KEY, terminal_name TEXT NOT NULL)',
         );
-        // terminals
-        db.execute(
-          'CREATE TABLE $_tripsTableName(trip_no INTEGER PRIMARY KEY, vehicle_no INTEGER, start_terminal_id INTEGER, end_terminal_id INTEGER, driver_no INTEGER, conductor_no INTEGER)',
-        );
+        // vehicles
+        db.execute('''CREATE TABLE $_vehiclesTableName(
+          vehicle_no INTEGER PRIMARY KEY, 
+          plate_no TEXT NOT NULL, 
+          capacity INT, 
+          vehicle_type TEXT, 
+          terminal_id INTEGER UNIQUE, 
+          FOREIGN KEY (terminal_id) REFERENCES $_terminalsTableName (terminal_id)
+          )''');
+        // drivers
+        db.execute('''CREATE TABLE $_driversTableName(
+          driver_no INTEGER PRIMARY KEY, 
+          first_name TEXT NOT NULL, 
+          last_name TEXT NOT NULL, 
+          age INTEGER
+          )''');
+        // conductors
+        db.execute('''CREATE TABLE $_conductorsTableName(
+          conductor_no INTEGER PRIMARY KEY, 
+          first_name TEXT NOT NULL, 
+          last_name TEXT NOT NULL, 
+          age INTEGER)''');
+
+        ///// TODO - these two tables will be updated later
+
+        // trips
+        db.execute('''CREATE TABLE $_tripsTableName(
+          trip_no INTEGER PRIMARY KEY, 
+          vehicle_no INTEGER, 
+          start_terminal_id INTEGER, 
+          end_terminal_id INTEGER, 
+          driver_no INTEGER, 
+          conductor_no INTEGER,
+          status TEXT,
+          available INTEGER,
+
+          FOREIGN KEY (vehicle_no) REFERENCES $_vehiclesTableName (vehicle_no),
+          FOREIGN KEY (start_terminal_id) REFERENCES $_terminalsTableName (terminal_id),
+          FOREIGN KEY (end_terminal_id) REFERENCES $_terminalsTableName (terminal_id),
+          FOREIGN KEY (driver_no) REFERENCES $_driversTableName (driver_no),
+          FOREIGN KEY (conductor_no) REFERENCES $_conductorsTableName (conductor_no),
+          )''');
       },
     );
     return database;
   }
 
-  void addVehicle(String plate, int capacity, String type) async {
+  //* DATABASE FUNCTIONS
+
+  void addTerminal(String? terminal_name) async {
+    final db = await database;
+    await db.insert(_terminalsTableName, {'terminal_name': terminal_name});
+  }
+
+  void addVehicle(
+    String plate,
+    int capacity,
+    String type,
+    int terminal_id,
+  ) async {
     final db = await database;
     await db.insert(_vehiclesTableName, {
       'plate_no': plate,
       'capacity': capacity,
       'vehicle_type': type,
+      'terminal_id': terminal_id,
+    });
+  }
+
+  void addDriver(String? first_name, String? last_name, int? age) async {
+    final db = await database;
+    await db.insert(_driversTableName, {
+      'first_name': first_name,
+      'last_name': last_name,
+      'age': age,
+    });
+  }
+
+  void addConductor(String? first_name, String? last_name, int? age) async {
+    final db = await database;
+    await db.insert(_conductorsTableName, {
+      'first_name': first_name,
+      'last_name': last_name,
+      'age': age,
+    });
+  }
+
+  void generateTrip(
+    int vehicle_no,
+    String start_terminal,
+    String end_terminal,
+    int driver_no,
+    int conductor_no,
+    String status,
+    int available,
+  ) async {
+    final db = await database;
+    await db.insert(_tripsTableName, {
+      'vehicle_no': vehicle_no,
+      'start_terminal_id': start_terminal,
+      'end_terminal_id': end_terminal,
+      'driver_no': driver_no,
+      'conductor_no': conductor_no,
+      'status': status,
+      'available': available,
     });
   }
 
@@ -80,7 +163,63 @@ class DatabaseService {
         row['plate_no'].toString(),
         row['capacity'].toString(),
         row['vehicle_type'].toString(),
+        row['terminal_id'].toString(),
       ];
     }).toList();
+  }
+
+  Future<List<List<String>>> getTerminals() async {
+    final db = await database;
+    final rawData = await db.query(_terminalsTableName);
+
+    return rawData.map((row) {
+      return [row['terminal_id'].toString(), row['terminal_name'].toString()];
+    }).toList();
+  }
+
+  Future<List<List<String>>> getDrivers() async {
+    final db = await database;
+    final rawData = await db.query(_driversTableName);
+
+    // convert to DbTables usable format
+    // Future<List<List<String>>>
+
+    return rawData.map((row) {
+      return [
+        row['driver_no'].toString(),
+        row['first_name'].toString(),
+        row['last_name'].toString(),
+        row['age'].toString(),
+      ];
+    }).toList();
+  }
+
+  Future<List<List<String>>> getConductors() async {
+    final db = await database;
+    final rawData = await db.query(_conductorsTableName);
+
+    // convert to DbTables usable format
+    // Future<List<List<String>>>
+
+    return rawData.map((row) {
+      return [
+        row['conductor_no'].toString(),
+        row['first_name'].toString(),
+        row['last_name'].toString(),
+        row['age'].toString(),
+      ];
+    }).toList();
+  }
+
+  //
+
+  Future<void> printDbPath() async {
+    final dbPath = await getDatabasesPath();
+    print(dbPath);
+  }
+
+  Future<List<Map<String, dynamic>>> getRawData() async {
+    final db = await database;
+    return await db.rawQuery('SELECT vehicle_no, plate_no FROM vehicles');
   }
 }
--- lib/pages/admin_page.dart
+++ lib/pages/admin_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:PTTS/database/database_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:PTTS/elements/db_tables.dart';
@@ -10,36 +12,55 @@ class AdminPage extends StatefulWidget {
   State<AdminPage> createState() => _AdminPageState();
 }
 
-// * functions
-void loadVehicles() async {
-  final rows = await DatabaseService.instance.getVehicles();
-  // i can't do setState here
-}
-
 class _AdminPageState extends State<AdminPage> {
   // define database access
   late final DatabaseService _databaseService = DatabaseService.instance;
+
+  // terminals
+  String? _terminal_name;
 
   // vehicles
   String? _plate;
   int _capacity = 0;
   String? _type;
+  int _terminal_id = 0;
+
+  // drivers
+  String? _driver_first_name;
+  String? _driver_last_name;
+  int? _driver_age;
+
+  // conductors
+  String? _conductor_first_name;
+  String? _conductor_last_name;
+  int? _conductor_age;
+
+  //==//==//==//==//==//==//==//==//==//==//
 
   // init variables
   List<List<String>> vehicleRows = [];
+  List<List<String>> driverRows = [];
+  List<List<String>> conductorRows = [];
+  List<List<String>> terminalRows = [];
 
   @override
   // init functions
   void initState() {
     super.initState();
-    loadVehicles();
+    loadData();
   }
 
   // * functions
-  void loadVehicles() async {
-    final rows = await DatabaseService.instance.getVehicles();
+  void loadData() async {
+    final _vehicles = await DatabaseService.instance.getVehicles();
+    final _drivers = await DatabaseService.instance.getDrivers();
+    final _conductors = await DatabaseService.instance.getConductors();
+    final _terminals = await DatabaseService.instance.getTerminals();
     setState(() {
-      vehicleRows = rows;
+      vehicleRows = _vehicles;
+      driverRows = _drivers;
+      conductorRows = _conductors;
+      terminalRows = _terminals;
     });
   }
 
@@ -71,7 +92,6 @@ class _AdminPageState extends State<AdminPage> {
                   child: Text('print'),
                   onPressed: () async {
                     //! add the dummy data here
-                    await _databaseService.getVehicles();
                   },
                 ),
                 // add new vehicle
@@ -82,6 +102,7 @@ class _AdminPageState extends State<AdminPage> {
                     'Plate',
                     'Capacity',
                     'Vehicle Type',
+                    'Terminal',
                   ],
                   tableRows: vehicleRows,
                   buttonPopup: CupertinoAlertDialog(
@@ -108,7 +129,7 @@ class _AdminPageState extends State<AdminPage> {
                           child: CupertinoTextField(
                             placeholder: 'Capacity',
                             textAlign: TextAlign.center,
-                            onSubmitted: (value) {
+                            onChanged: (value) {
                               _capacity = int.parse(value);
                             },
                           ),
@@ -123,6 +144,16 @@ class _AdminPageState extends State<AdminPage> {
                             },
                           ),
                         ),
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: CupertinoTextField(
+                            placeholder: 'terminal_id',
+                            textAlign: TextAlign.center,
+                            onChanged: (value) {
+                              _terminal_id = int.parse(value);
+                            },
+                          ),
+                        ),
                         CupertinoButton(
                           child: Text('Add'),
                           onPressed: () {
@@ -134,6 +165,7 @@ class _AdminPageState extends State<AdminPage> {
                               _plate.toString(),
                               _capacity,
                               _type.toString(),
+                              _terminal_id,
                             );
 
                             // set variables back to null
@@ -144,6 +176,7 @@ class _AdminPageState extends State<AdminPage> {
                             _plate = null;
                             _capacity = 0;
                             _type = null;
+                            loadData();
                             Navigator.pop(context);
                           },
                         ),
@@ -161,7 +194,88 @@ class _AdminPageState extends State<AdminPage> {
                     'Last Name',
                     'Age',
                   ],
-                  tableRows: drivers,
+                  tableRows: driverRows,
+                  buttonPopup: CupertinoAlertDialog(
+                    title: Text("Add Driver"),
+                    content: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 16,
+                            bottom: 4,
+                            left: 4,
+                            right: 4,
+                          ),
+                          child: CupertinoTextField(
+                            placeholder: 'First Name',
+                            textAlign: TextAlign.center,
+                            onChanged: (value) {
+                              _driver_first_name = value;
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 4,
+                            bottom: 4,
+                            left: 4,
+                            right: 4,
+                          ),
+                          child: CupertinoTextField(
+                            placeholder: 'Last Name',
+                            textAlign: TextAlign.center,
+                            onChanged: (value) {
+                              _driver_last_name = value;
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 4,
+                            bottom: 4,
+                            left: 4,
+                            right: 4,
+                          ),
+                          child: CupertinoTextField(
+                            placeholder: 'Age',
+                            textAlign: TextAlign.center,
+                            onChanged: (value) {
+                              _driver_age = int.parse(value);
+                            },
+                          ),
+                        ),
+
+                        CupertinoButton(
+                          child: Text('Add'),
+                          onPressed: () {
+                            // check if plate or type is null
+                            // do nothing if null
+                            if (_driver_first_name == null ||
+                                _driver_last_name == null)
+                              return;
+                            // else insert to table
+                            _databaseService.addDriver(
+                              _driver_first_name,
+                              _driver_last_name,
+                              _driver_age,
+                            );
+
+                            // set variables back to null
+                            // for insertion again later
+                            print(
+                              'pushed $_driver_first_name, $_driver_last_name, $_driver_age to table',
+                            );
+                            _driver_first_name = null;
+                            _driver_last_name = null;
+                            _driver_age = null;
+
+                            loadData();
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
 
                 // add new conductors
@@ -173,14 +287,134 @@ class _AdminPageState extends State<AdminPage> {
                     'Last Name',
                     'Age',
                   ],
-                  tableRows: conductors,
+                  tableRows: conductorRows,
+                  buttonPopup: CupertinoAlertDialog(
+                    title: Text("Add Conductor"),
+                    content: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 16,
+                            bottom: 4,
+                            left: 4,
+                            right: 4,
+                          ),
+                          child: CupertinoTextField(
+                            placeholder: 'First Name',
+                            textAlign: TextAlign.center,
+                            onChanged: (value) {
+                              _conductor_first_name = value;
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 4,
+                            bottom: 4,
+                            left: 4,
+                            right: 4,
+                          ),
+                          child: CupertinoTextField(
+                            placeholder: 'Last Name',
+                            textAlign: TextAlign.center,
+                            onChanged: (value) {
+                              _conductor_last_name = value;
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 4,
+                            bottom: 4,
+                            left: 4,
+                            right: 4,
+                          ),
+                          child: CupertinoTextField(
+                            placeholder: 'Age',
+                            textAlign: TextAlign.center,
+                            onChanged: (value) {
+                              _conductor_age = int.parse(value);
+                            },
+                          ),
+                        ),
+
+                        CupertinoButton(
+                          child: Text('Add'),
+                          onPressed: () {
+                            // check if plate or type is null
+                            // do nothing if null
+                            if (_conductor_first_name == null ||
+                                _conductor_last_name == null)
+                              return;
+                            // else insert to table
+                            _databaseService.addConductor(
+                              _conductor_first_name,
+                              _conductor_last_name,
+                              _conductor_age,
+                            );
+
+                            // set variables back to null
+                            // for insertion again later
+                            print(
+                              'pushed $_conductor_first_name, $_conductor_last_name, $_conductor_age to table',
+                            );
+                            _conductor_first_name = null;
+                            _conductor_last_name = null;
+                            _conductor_age = null;
+
+                            loadData();
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
 
                 // add new terminals
                 DbTables(
                   tableTitle: 'Terminals',
-                  tableColumns: ['Type', 'Location'],
-                  tableRows: terminals,
+                  tableColumns: ['Terminal ID', 'Location'],
+                  tableRows: terminalRows,
+                  buttonPopup: CupertinoAlertDialog(
+                    title: Text("Add Terminal"),
+                    content: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 16,
+                            bottom: 4,
+                            left: 4,
+                            right: 4,
+                          ),
+                          child: CupertinoTextField(
+                            placeholder: 'Terminal Name',
+                            textAlign: TextAlign.center,
+                            onChanged: (value) {
+                              _terminal_name = value;
+                            },
+                          ),
+                        ),
+
+                        CupertinoButton(
+                          child: Text('Add'),
+                          onPressed: () {
+                            // check if plate or type is null
+                            // do nothing if null
+                            if (_terminal_name == null) return;
+                            // else insert to table
+                            _databaseService.addTerminal(_terminal_name);
+
+                            // set variables back to null
+                            // for insertion again later
+                            print('pushed $_terminal_name to table');
+                            _terminal_name = null;
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
